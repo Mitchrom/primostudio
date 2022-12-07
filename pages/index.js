5fc9aa06e010ce71a14581d1a5ae7ec65a20a4b9
Mitchrom <romaricmitch@gmail.com>
2022-12-07 17:43:40 -0300

    deuxièmme commit

--- pages/index.js
+++ pages/index.js
@@ -25,11 +25,44 @@ import "swiper/css/autoplay";
 import { HeaderDesktop, HeaderMobile } from "../Components/Header";
 import { Partenaires } from "../Components/item/Partenaires";
 
-export default function Home() {
+import { request } from "./api/datocms";
+
+const HOMEPAGE_QUERY = `
+query MyQuery {
+  allServiceCards {
+    iconeService {
+      url
+    }
+    service {
+      service
+    }
+    caption1
+    caption2
+    caption3
+    caption4
+    bgColor {
+      hex
+    }
+  }
+}
+`;
+export async function getStaticProps() {
+  const data = await request({
+    query: HOMEPAGE_QUERY,
+  });
+  return {
+    props: { data },
+  };
+}
+
+export default function Home(props) {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
   const [ajd, setAjd] = useState(new Date());
   const [menuClicked, setMenuClicked] = useState(false);
+
+  const { data } = props;
+  const cards = data.allServiceCards;
 
   setTimeout(() => {
     setAjd(new Date());
@@ -47,6 +80,7 @@ export default function Home() {
   useEffect(() => {
     setMounted(true);
     setMenuClicked(false);
+    console.log(cards);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -206,11 +240,49 @@ export default function Home() {
         </div>
 
         <div className="flex flex-row flex-wrap justify-center">
-          <div className="w-[340px] h-[195px] m-4 bg-red-300" />
-          <div className="w-[340px] h-[195px] m-4 bg-sky-300" />
-          <div className="w-[340px] h-[195px] m-4 bg-green-300" />
-          <div className="w-[340px] h-[195px] m-4 bg-indigo-300" />
-          <div className="w-[340px] h-[195px] m-4 bg-yellow-300" />
+          {cards.map((card, i) => (
+            <div
+              key={i}
+              className="flex flex-row shadow-xl my-4 w-[340px] h-[150px] mx-auto"
+            >
+              <div
+                style={{ backgroundColor: card.bgColor.hex }}
+                className="flex flex-col justify-around min-w-[120px] max-w-[120px]"
+              >
+                <div>
+                  <Image
+                    src={card.iconeService.url}
+                    alt={"icone service " + card.service.service}
+                    width={24}
+                    height={24}
+                    loading="lazy"
+                  />
+                  <p className="text-[15px] font-bold">
+                    {card.service.service}
+                  </p>
+                </div>
+                <div className="flex justify-center">
+                  <button className="w-3/4">
+                    <p className="text-xs">En savoir plus {">"}</p>
+                  </button>
+                </div>
+              </div>
+              <div className="flex flex-col self-center min-w-[225px] max-w-[225px]">
+                <p className="text-xs font-medium mx-2 my-1 text-start">
+                  {card.caption1}
+                </p>
+                <p className="text-xs font-medium mx-2 my-1 text-start">
+                  {card.caption2}
+                </p>
+                <p className="text-xs font-medium mx-2 my-1 text-start">
+                  {card.caption3}
+                </p>
+                <p className="text-xs font-medium mx-2 my-1 text-start">
+                  {card.caption4}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
 
         <div className="my-4">
